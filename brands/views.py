9d2b63d046ba8dcc6326fcc4342e660af214eb71
Luis Gustavo Correia <139191105+LuisGustavoCo@users.noreply.github.com>
2025-08-07 22:19:23 -0300

    suppliers, categories and brands. Changes in sidebarhtml too

--- brands/views.py
+++ brands/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, CreateView, DetailView, UpdateView, D
 from . import models, forms
 from django.urls import reverse_lazy
 
+
 class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
@@ -17,21 +18,25 @@ class BrandListView(ListView):
 
         return queryset
     
+
 class BrandCreateView(CreateView):
     model = models.Brand
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
 
+
 class BrandDetailView(DetailView):
     model = models.Brand
     template_name = 'brand_detail.html'
+
 
 class BrandUpdateView(UpdateView):
     model = models.Brand
     template_name = 'brand_update.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+    
     
 class BrandDeleteView(DeleteView):
     model = models.Brand
